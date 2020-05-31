01b4103c08459eac0b78ac16d286de4c6054d091
Ted Liang <teddyliang@gmail.com>
2020-05-31 10:31:20 +1000

    Upgrade Java to 11

--- vars.sh
+++ vars.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export JRE_URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz
+export JRE_URL=https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.7%2B10/OpenJDK11U-jre_x64_linux_hotspot_11.0.7_10.tar.gz
 export SONARQUBE_DIST_URL="https://binaries.sonarsource.com/Distribution"
 
 export SONARQUBE_ZIP="sonarqube-${SONARQUBE_VERSION}.zip"
